8695e236e2622ad36af1a8e0c4c69cf9a3f27a67
YanaTsiFi <yanagrischechko@gmail.com>
2025-02-26 12:53:15 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,6 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello qa guru!");
+        System.out.printf("Hello qa guru!!!!!!");
     }
 }
